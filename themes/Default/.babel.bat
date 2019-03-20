1adbcedddf0f8593444a8cca7c74307068914c61
Al Azif <alazif@miskatonic.io>
2019-03-19 18:42:15 -0700

    Update

--- themes/Default/.babel.bat
+++
@@ -1,5 +0,0 @@
-call npm install --save-dev @babel/core @babel/cli @babel/preset-env babel-preset-minify
-call babel default.js.es6 --out-file default.js --presets=@babel/preset-env,minify --no-comments
-call babel common.js.es6 --out-file common.js --presets=@babel/preset-env,minify --no-comments
-call babel rpi.js.es6 --out-file rpi.js --presets=@babel/preset-env,minify --no-comments
-del package-lock.json
